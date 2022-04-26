7369563eb7abddc12fa0290122fc9820379e4bd6
symphony <jciurc@gmail.com>
2022-04-26 15:34:05 -0600

    adds basic jquery functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,17 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 $(document).ready(()=> {
-  // run all dom functions in here
-console.log('doc ready');
+  $('#tweet-form').on('submit', (event) => {
+    event.preventDefault();
+    alert('submitted');
+  });
+
+  $('#tweet-button').on('click', ({target}) => {
+    setTimeout(() => {
+      target.blur();
+    }, 200);
+
+
+  });
+
+
+
 });
--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 const PORT          = process.env.PORT || 3000;
+
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
+
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
 // actual database it uses and see little to no changes elsewhere in the code
@@ -13,8 +15,10 @@ const db = require("./lib/in-memory-db");
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
 const DataHelpers = require("./lib/data-helpers.js")(db);
+
 // Update the dates for the initial tweets (data-files/initial-tweets.json).
 require("./lib/date-adjust")();
+
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
@@ -22,12 +26,9 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // == middleware ==
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
-
-app.post('/tweets/', (req, res) => {
-console.log('did it work', req.body.text);
-});
 
 // == start server ==
 app.listen(PORT, () => {
