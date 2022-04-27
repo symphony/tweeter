026beb10a00edeac430a4c8f29a836e9c1d3f2b9
symphony <jciurc@gmail.com>
2022-04-27 14:00:06 -0600

    adds date-fns. sets up basic tweet card html template in client.js

--- README.md
+++ README.md
@@ -6,13 +6,18 @@ This repository is the starter code for the project: Students will fork and clon
 
 ## Getting Started
 
-1. [Create](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) a new repository using this repository as a template.
-2. Clone your repository onto your local device.
-3. Install dependencies using the `npm install` command.
-3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
-4. Go to <http://localhost:8080/> in your browser.
+1. Clone your repository onto your local device.  
+`git clone git@github.com:symphony/tinyapp.git && cd tweeter`  
+2. Install dependencies using the `npm install` command.
+3. Start the web server using the `npm start` command.
+4. Go to <http://localhost:3000/> in your browser.
 
 ## Dependencies
 
 - Express
 - Node 5.10.x or above
+- Body-Parser
+- Chance
+- Date-FNS
+- MD5
+- (Dev) Nodemon
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,69 @@
+import { formatDistance, subDays } from 'date-fns'
+
+const buildTweetCard= ({user, content, created_at}) => {
+  const timeAgo = formatDistance(subDays(new Date(created_at), 3), new Date(), { addSuffix: true });
+  const htmlStructure =
+`
+<article class="flex column feed-card">
+  <header class="flex user-info">
+    <div class="flex user-avatar">
+      <img src="${user.avatars}" class="avatar"></img>
+      <span class="username">${user.name}</span>
+    </div>
+      <span class="handle"><strong>${user.handle}</strong></span>
+  </header>
+
+  <span class="tweet-content">${content.text}</span>
+    <div class="flex footer">
+      <span class="timestamp">${timeAgo}</span>
+      <span class="socials">
+        <i class="social fa-solid fa-flag"></i>
+        <i class="social fa-solid fa-retweet"></i>
+        <i class="social fa-solid fa-heart"></i>
+      </span>
+    </div>
+</article>
+`;
+return htmlStructure;
+};
+
 $(document).ready(() => {
   $('#nav-cta').on('click', () => {
     setTimeout(() => {
       $('#text-area').focus();
     }, 10)
+
+    const newTweet = $.ajax('/tweets').slice(-1)[0];
+    $('#feed-container').prepend(buildTweetCard(newTweet))
+
+
   });
 });
+
+$(() => {
+  const { user, content, created_at: timestamp } = { "user": { "name": "Descartes", "avatars": "https://i.imgur.com/nlhLi3I.png", "handle": "@rd" }, "content": { "text": "Je pense , donc je suis" }, "created_at": 1650934142888 };
+});
+
+`<article class="feed-card rounded">
+  <div class="user">
+    <span>
+      <div>
+        <img src="user.avatars" class="avatar"></img>
+        <p class="username">user.name</p>
+      </div>
+      <p class="handle">user.handle</p>
+    </span>
+  </div>
+
+  <div class="content">content.text</div>
+  <div>
+    <span>
+      <p class="timestamp">new Date(timestamp)</p>
+      <div class="socials">
+        <i class="fa-duotone fa-camera-retro"></i>
+        <i class="fa-duotone fa-camera-retro"></i>
+        <i class="fa-duotone fa-camera-retro"></i>
+      </div>
+    </span>
+  </div>
+</article>`
