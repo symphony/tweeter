a021179e743df18ba3b75ea9ba4dba9614620b5e
symphony <jciurc@gmail.com>
2022-04-29 17:02:59 -0600

    refactors scripts. extracts functions to outside of document ready

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,123 +1,124 @@
-const { distanceInWordsToNow } = dateFns;
+(() => {
+  const { distanceInWordsToNow } = dateFns;
 
-// == helpers ==
-const safeText = (text) => {
-  const safe = document.createElement("div");
-  safe.appendChild(document.createTextNode(text));
-  return safe.innerHTML;
-};
-
-const createTweetElement = ({user, content, created_at}) => {
-  const timeAgo = distanceInWordsToNow(new Date(created_at), new Date(), { addSuffix: true }) + ' ago';
-  const htmlStructure = // need to create escape function for xss vulnerabilities
-`
-<article class="flex column tweet">
-  <header class="flex">
-    <span class="flex">
-      <img src="${user.avatars}"></img>
-      <span class=>${safeText(user.name)}</span>
-    </span>
-    <span><strong>${safeText(user.handle)}</strong></span>
-  </header>
-  <p>${safeText(content.text)}</p>
-    <footer class="flex">
-      <span class="timestamp">${timeAgo}</span>
-      <span class="socials">
-        <i class="social fa-solid fa-flag"></i>
-        <i class="social fa-solid fa-retweet"></i>
-        <i class="social fa-solid fa-heart"></i>
-      </span>
-    </footer>
-</article>
-`;
-  return htmlStructure;
-};
-
-const renderTweets = (tweets) => {
-  $('#feed-container').html('');
-  for (const tweet of tweets) {
-    $('#feed-container').prepend(createTweetElement(tweet));
-  }
-};
-
-const loadTweets = () => {
-  $.get('/tweets')
-  .then((data) => {
-    renderTweets(data);
-  })
-  .catch((error) => {
-    alert("Error fetching. See console log.");
-    console.log(error);
-  });
-};
-
-const submitTweet = (textSerialized, loadTweets) => {
-  $.post('/tweets', textSerialized)
-    .then((data) => {
-      loadTweets();
-    })
-    .catch((error) => {
-      alert("Error fetching. See console log.");
-      console.log(error);
+  $(document).ready(() => {
+    // Reset (any) button state on click
+    $('button').on('click', ({target}) => {
+      target.blur();
     });
-};
 
-const showAlert = (message) => {
-  const $alert = $('#alert-box');
-  $alert.children('.alert').children('.alert-text').text(message);
-  $alert.slideDown();
-  setTimeout(() => {
-    $alert.slideUp();
-  }, 6000);
-};
+    $('#nav-cta').on('click', goToComposeTweet);
 
-// =================
-// == page loaded ==
-// =================
-$(document).ready(() => {
-  // Reset (any) button state on click
-  $('button').on('click', ({target}) => {
-    target.blur();
+    $('.alert-close').click(function() {
+      $(this).parent().parent().slideUp();
+    });
+
+    $('#compose-tweet').on('submit', onSubmit);
+
+    // == initial page load behaviour ==
+    loadTweets();
   });
 
-  $('#nav-cta').on('click', () => {
-    // removes extra text in url bar
-    const url = window.location.href.split(/[?#]/)[0];
-    setTimeout(() => {
-      $('#text-area').focus();
-      window.history.pushState({}, null, url);
-    }, 10)
-  });
 
-  $('.alert-close').click(function() {
-    $(this).parent().parent().slideUp();
-  });
-
-  // Compose tweet submission
-  $('#compose-tweet').submit(function(event) {
+  // =============
+  // == helpers ==
+  // =============
+  const onSubmit = function(event) {
     event.preventDefault();
-    const $textarea = $(this).children('section').children('textarea');
-    const $counter = $(this).children('section').children('footer').children('output');
+    const $textarea = $(this).find('textarea');
+    const $counter = $(this).find('output');
     const textSerialized = $textarea.serialize();
     const textPlain = $textarea.val().trimStart(); // we'll ignore leading whitespace, but not trailing
     const charLimit = 140;
     $textarea.focus();
 
     // form validation
-    if (textPlain.length > charLimit) return showAlert("Tweet exceeds character limit");
+    if (textPlain.length > charLimit)
+      return showAlert("Tweet exceeds character limit");
     // reset input field
     $textarea.val('');
-    $counter.removeClass('text-orange').removeClass('text-red').text(charLimit);
-    if (!textPlain) return showAlert("Tweet cannot be empty");
+    $counter.removeClass('text-orange text-red').text(charLimit);
+    if (!textPlain)
+      return showAlert("Tweet cannot be empty");
 
     // success
     submitTweet(textSerialized, loadTweets);
-  });
+  };
 
-  // == initial page load behaviour ==
-  loadTweets();
-});
+  const submitTweet = (textSerialized, loadTweets) => {
+    $.post('/tweets', textSerialized)
+      .then(loadTweets)
+      .catch((error) => {
+        alert("Error fetching. See console log.");
+        console.log(error);
+      });
+  };
 
+  const goToComposeTweet = () => {
+    // removes extra text in url bar
+    const url = window.location.href.split(/[?#]/)[0];
+    setTimeout(() => {
+      $('#text-area').focus();
+      window.history.pushState({}, null, url);
+    }, 10)
+  }
 
+  const safeText = (text) => {
+    const safe = document.createElement("div");
+    safe.appendChild(document.createTextNode(text));
+    return safe.innerHTML;
+  };
 
-$(this).parents('div').parents('div').siblings('nav').children('div')
+  const createTweetElement = ({user, content, created_at}) => {
+    const timeAgo = distanceInWordsToNow(new Date(created_at), new Date(), { addSuffix: true }) + ' ago';
+    const htmlStructure =
+  `
+  <article class="flex column tweet">
+    <header class="flex">
+      <span class="flex">
+        <img src="${user.avatars}"></img>
+        <span class=>${safeText(user.name)}</span>
+      </span>
+      <span><strong>${safeText(user.handle)}</strong></span>
+    </header>
+    <p>${safeText(content.text)}</p>
+      <footer class="flex">
+        <span class="timestamp">${timeAgo}</span>
+        <span class="socials">
+          <i class="social fa-solid fa-flag"></i>
+          <i class="social fa-solid fa-retweet"></i>
+          <i class="social fa-solid fa-heart"></i>
+        </span>
+      </footer>
+  </article>
+  `;
+    return htmlStructure;
+  };
+
+  const renderTweets = (tweets) => {
+    $('#feed-container').html('');
+    for (const tweet of tweets) {
+      $('#feed-container').prepend(createTweetElement(tweet));
+    }
+  };
+
+  const loadTweets = () => {
+    $.get('/tweets')
+    .then((data) => {
+      renderTweets(data);
+    })
+    .catch((error) => {
+      alert("Error fetching. See console log.");
+      console.log(error);
+    });
+  };
+
+  const showAlert = (message) => {
+    const $alert = $('#alert-box');
+    $alert.children('.alert').children('.alert-text').text(message);
+    $alert.slideDown();
+    setTimeout(() => {
+      $alert.slideUp();
+    }, 6000);
+  };
+})();
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,16 @@
-$(document).ready(() => {
-  const $counter = $('#counter');
-  const charLimit = 140;
-  let charCount = 0;
+(() => {
+  $(document).ready(() => {
+    const $counter = $('#counter');
+    const charLimit = 140;
+    let charCount = 0;
 
-  $('#text-area').on('input propertychange', function() {
-    charCount = $(this).val().trimStart().length;
-    $counter.text(charLimit - charCount);
-    if (charCount >= 100) $counter.addClass('text-orange');
-    if (charCount <= 99) $counter.removeClass('text-orange');
-    if (charCount > charLimit) $counter.addClass('text-red');
-    if (charCount <= charLimit) $counter.removeClass('text-red');
+    $('#text-area').on('input propertychange', function() {
+      charCount = $(this).val().trimStart().length;
+      $counter.text(charLimit - charCount);
+      if (charCount >= 100) $counter.addClass('text-orange');
+      if (charCount <= 99) $counter.removeClass('text-orange');
+      if (charCount > charLimit) $counter.addClass('text-red');
+      if (charCount <= charLimit) $counter.removeClass('text-red');
+    });
   });
-});
+})();
